85b8aef6d6c6c892e3badb1f849d8c42dbbbd52c
Sergey Balalaev <sof.bix@mail.ru>
2019-11-07 19:32:39 +0400

    init project for the task #2

---
+++ 02/skybonds/Main/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  skybonds
+//
+//  Created by Sergey Balalaev on 07.11.2019.
+//  Copyright © 2019 Altarix. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
---
+++ 02/skybonds/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  skybonds
+//
+//  Created by Sergey Balalaev on 07.11.2019.
+//  Copyright © 2019 Altarix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
